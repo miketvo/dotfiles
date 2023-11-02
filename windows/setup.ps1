8e91ad71e0d0b592b4b1c6ca8b9c19514eb7427e
miketvo <miketvo@outlook.com>
2023-11-02 15:47:45 +0700

    Updated Windows setup script

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,9 +1,9 @@
 # Get Packages
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
-scoop bucket add anderlli0053_DEV-tools https://github.com/anderlli0053/DEV-tools.git
 scoop bucket add extras
-scoop install main/git main/gh main/curl main/coreutils main/which main/touch main/bottom main/bat main/mdcat main/broot main/fd main/fzf main/winfetch extras/wezterm main/oh-my-posh extras/onefetch extras/lazygit anderlli0053_DEV-tools/exa
+scoop bucket add anderlli0053_DEV-tools https://github.com/anderlli0053/DEV-tools.git
+scoop install main/git main/gh main/curl main/coreutils main/which main/touch main/bottom main/bat main/mdcat main/broot main/fd main/fzf main/winfetch extras/wezterm extras/windows-terminal main/oh-my-posh extras/onefetch extras/lazygit anderlli0053_DEV-tools/exa
 scoop install main/neovim
 iwr -useb "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" |`
     ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
