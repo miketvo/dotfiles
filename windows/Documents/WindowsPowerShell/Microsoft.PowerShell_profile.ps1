b8b605a5b89ebdd2b6b3a58db3530a60390d3ca6
miketvo <miketvo@outlook.com>
2023-11-02 16:08:50 +0700

    Updated RUVN Lab setup

--- windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,3 @@
-# Predictive IntelliSense (auto-suggestions)
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
-$options = Get-PSReadlineOption
-$options.InlinePredictionColor = 'DarkGray'
-Set-PSReadLineKeyHandler -Chord 'Shift+Spacebar' -Function AcceptNextSuggestionWord
-Set-PSReadLineKeyHandler -Chord 'Ctrl+Spacebar' -Function AcceptSuggestion
-
 # Load oh-my-posh config
 oh-my-posh init pwsh --config ~\.oh-my-posh.omp.json | Invoke-Expression
 
