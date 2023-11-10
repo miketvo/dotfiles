f01a57cb0d9e370a41ab1572ad7e02380018b801
miketvo <miketvo@outlook.com>
2023-11-10 08:15:12 +0700

    More verbose Windows setup script

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,4 +1,5 @@
 # Scoop Setup
+Write-Output "Setting up Scoop package manager..."
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
 scoop bucket add extras
@@ -7,6 +8,7 @@ scoop bucket add java
 scoop bucket add anderlli0053_DEV-tools "https://github.com/anderlli0053/DEV-tools.git"
 
 # Essential Packages
+Write-Output "Installing essential packages..."
 scoop install main/aria2
 scoop install main/git
 scoop install main/gh
@@ -34,16 +36,19 @@ scoop install anderlli0053_DEV-tools/du
 scoop install anderlli0053_DEV-tools/exa
 
 # Neovim
+Write-Output "Installing NeoVim..."
 scoop install main/neovim
 iwr -useb "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" |`
     ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 
 # NodeJS Ecosystem
+Write-Output "Setting up NodeJS ecosystem..."
 scoop install main/nvm
 nvm install lts
 nvm use lts
 
 # Python Ecosystem
+Write-Output "Setting up Python ecosystem..."
 scoop install main/python
 python -m pip install pipx
 python -m pipx install pipenv
@@ -51,19 +56,25 @@ python -m pipx install cookiecutter
 python -m pipx ensurepath
 
 # C/C++
+Write-Output "Setting up C/C++ ecosystem..."
 scoop install main/gcc
 scoop install main/gdb
 scoop install main/llvm
 
 # Java
+Write-Output "Setting up Java ecosystem..."
 scoop install java/openjdk
 scoop install main/jdtls
 
 # Nerd Fonts
+Write-Output "Installing Nerd Fonts..."
 git clone --filter=blob:none --sparse "https://github.com/ryanoasis/nerd-fonts.git"
 git -C nerd-fonts sparse-checkout add patched-fonts/JetBrainsMono
 nerd-fonts/install.ps1 JetBrainsMono -WhatIf
 rm -r -Force nerd-fonts
 
 # Install Configuration Files
+Write-Output "Installing configurations..."
 Copy-Item -Path "$PSScriptRoot/*" -Destination "$env:USERPROFILE" -Exclude "setup.ps1" -Recurse -Force -Container
+
+Write-Output "[ DONE ]"
