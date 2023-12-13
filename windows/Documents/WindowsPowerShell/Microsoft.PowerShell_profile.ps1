42dc57bb4aa974b95a7b6fc89ddb0c72e24f1084
miketvo <miketvo@outlook.com>
2023-12-13 19:41:48 +0700

    Windows: Migrage to Eza instead of Exa because of the latter's deprecation

--- windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -10,7 +10,7 @@ Set-PSReadLineKeyHandler -Chord 'Ctrl+Spacebar' -Function AcceptSuggestion
 oh-my-posh init pwsh --config ~\.oh-my-posh.omp.json | Invoke-Expression
 
 # Aliases
-Set-Alias -Name 'dr' -Value 'exa'
+Set-Alias -Name 'dr' -Value 'eza'
 Set-Alias -Name 'br' -Value 'broot'
 del alias:sl -Force
 
