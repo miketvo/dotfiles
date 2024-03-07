b00387d6c65649df397becdf7cdae5efb27e4e84
miketvo <miketvo@outlook.com>
2024-03-08 00:03:16 +0700

    Added VSCode to install script

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -47,6 +47,9 @@ scoop install main/neovim
 iwr -useb "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" |`
     ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 
+# Visual Studio Code
+scoop install extras/vscode
+
 # NodeJS Ecosystem
 Write-Output "Setting up NodeJS ecosystem..."
 scoop install main/nvm
