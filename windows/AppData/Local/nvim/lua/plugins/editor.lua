6fa9705dcb4b8ee465d502405b8a1132732dabe9
miketvo <miketvo@outlook.com>
2024-07-03 01:05:32 +0700

    Fix typo

--- windows/AppData/Local/nvim/lua/plugins/editor.lua
+++ windows/AppData/Local/nvim/lua/plugins/editor.lua
@@ -204,7 +204,7 @@ return {
       vim.cmd.highlight('NeoTreeFileStatsHeader guifg=#4C566A')          -- nord3.
       vim.cmd.highlight('NeoTreeFileStats guifg=#4C566A')                -- nord3.
       vim.cmd.highlight('NeoTreeMessage guifg=#4C566A')                  -- nord3.
-      vim.cmd.highlight('NeoTreeFloatBorder ctermfg=Gray guifg=#252931') -- no border.
+      vim.cmd.highlight('NeoTreeFloatBorder ctermfg=Black guifg=#252931') -- no border.
       vim.cmd.highlight('NeoTreeFloatTitle gui=bold guibg=#252931')      -- blended.
       vim.cmd.highlight('NeoTreeTitleBar gui=bold guibg=#252931')        -- blended.
     end,
