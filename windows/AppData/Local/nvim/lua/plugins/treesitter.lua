001e5d831758a7859b4be85c7b1612cf05f2fb73
miketvo <miketvo@outlook.com>
2024-07-02 19:45:54 +0700

    Updated more dependencies for neovim

--- windows/AppData/Local/nvim/lua/plugins/treesitter.lua
+++ windows/AppData/Local/nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,17 @@ return {
     'nvim-treesitter/nvim-treesitter',
     version = false, -- Last release is way too old and doesn't work on Windows.
     build = ':TSUpdate',
+    event = { 'BufReadPost', 'BufWritePost', 'BufNewFile', 'VeryLazy' },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline.
+    init = function(plugin)
+      -- PERF: Add nvim-treesitter queries to the rtp and it's custom query predicates early.
+      --
+      -- This is needed because a bunch of plugins no longer `require('nvim-treesitter')`, which no longer trigger the
+      -- **nvim-treesitter** module to be loaded in time. Luckily, the only things that those plugins need are the
+      -- custom queries, which we make available during startup.
+      require('lazy.core.loader').add_to_rtp(plugin)
+      require('nvim-treesitter.query_predicates')
+    end,
     cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
     opts = {
       ensure_installed = {
