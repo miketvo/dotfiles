2c0718835d3f6322f36dce7a2e684b9b744eb7e9
miketvo <miketvo@outlook.com>
2025-11-07 03:56:55 +0700

    Polished keybind cheatsheet menu for archlinux-wsl

--- archlinux-wsl/.config/rofi/launch-keybinds.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-exec rofi -show keybinds -window-format "Workspace {w}  {c} {t}" -theme-str "listview { columns: 1; }" -sidebar-mode -modes keybinds,keys -display-keys "launcher_keybinds"
