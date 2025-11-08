1ba6b5ae28dcd52bdd385f5f98742996e4228562
miketvo <miketvo@outlook.com>
2025-11-08 15:46:10 +0700

    archlinux-wsl: added clear workspace keybinding

---
+++ archlinux-wsl/.config/i3/i3ipc/kill_workspace.py
@@ -0,0 +1,20 @@
+from i3ipc import Connection
+
+
+def get_current_workspace(i3):
+    tree = i3.get_tree()
+    return tree.find_focused().workspace()
+
+
+def main():
+    i3 = Connection()
+    current_workspace = get_current_workspace(i3)
+    if current_workspace:
+        for leaf in current_workspace.leaves():
+            i3.command(f"[con_id={leaf.id}] kill")
+    else:
+        return
+
+
+if __name__ == "__main__":
+    main()
